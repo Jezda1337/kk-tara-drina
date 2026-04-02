f16ec784b931ea98f9c372ab0b4b4e617a0aa902
Radoje <megasrbin1@gmail.com>
2026-04-03 00:06:59 +0200

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  i18n: {
+    locales: ["rs", "en"],
+    defaultLocale: "rs",
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
+      styles: ["normal", "italic"],
+    },
+  ],
+});
+
